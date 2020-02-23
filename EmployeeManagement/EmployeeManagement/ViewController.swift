c65366f703e1742b47362ea46cad5891f666fc14
Mayur Kamthe <mayur.kamthe@wuerth-phoenix.com>
2020-02-23 20:12:57 +0100

    ADD initial commit
    initial project created and pushed to git

---
+++ EmployeeManagement/EmployeeManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmployeeManagement
+//
+//  Created by Mayur Kamthe on 23/02/20.
+//  Copyright © 2020 Mayur Kamthe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
